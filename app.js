5e1b9d8cc9ea7e639c66e41fdec9aa3127c546c5
VEMULA-MOUNITHA <vemulamounitha09@gmail.com>
2023-04-20 22:08:45 -0500

    added account file

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ var mongoose = require('mongoose')
 var mongodb = require('mongodb')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var gadget = require("./models/gadget");
+var gadget = require("./models/gadget");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 
 require('dotenv').config();
 const connectionString =
@@ -46,12 +61,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gadget', gadgetsRouter);
 app.use('/board', boardsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
