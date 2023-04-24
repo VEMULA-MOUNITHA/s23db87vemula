b2cddfdd280d02972fc714de28a326c7f78cf9a2
VEMULA-MOUNITHA <vemulamounitha09@gmail.com>
2023-04-24 18:42:09 -0500

    added bonus

--- models/gadget.js
+++ models/gadget.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const gadgetSchema = mongoose.Schema({
-    gadget_type: String,
-    gadget_price: Number,
+    gadget_type:{
+        type: String,
+        required: [true, "Name required"]
+    },
+    gadget_price:{
+        type: Number,
+        min: [9, "Min value is 9"],
+        max: [200, "Max value is 200"]
+    },
     gadget_version: String
 })
 module.exports = mongoose.model("gadget",gadgetSchema)
--- routes/gadget.js
+++ routes/gadget.js
@@ -4,15 +4,24 @@ var router = express.Router();
 
 router.get('/', gadget_controlers.gadget_view_all_Page );
 module.exports = router;
-
-
 // router.get('/detail', gadget_controlers.gadget_detail);
 
 router.get('/gadgets', gadget_controlers.gadget_list);
 
 router.get('/detail/', gadget_controlers.gadget_detail);
 
-router.get('/create', gadget_controlers.gadget_create_Page);
-router.get('/update', gadget_controlers.gadget_update_Page);
-router.get('/delete', gadget_controlers.gadget_delete_Page);
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+router.get('/update', secured,gadget_controlers.gadget_update_Page);
+router.get('/delete',secured, gadget_controlers.gadget_delete_Page);
+router.get('/create',secured, gadget_controlers.gadget_create_Page);
 module.exports = router;
